328e2d422460aa61b35fe5b6e194b578b818c151
Sean Billig <sean.billig@gmail.com>
2013-12-04 18:07:03 -0600

    Dict and vector packing

--- src/Msgpack.jl
+++ src/Msgpack.jl
@@ -15,8 +15,8 @@ pack(v) = begin
     takebuf_array(s)
 end
 
-pack(s, nothing) = write(s, 0xc0)
-pack(s, v::Bool) = if v write(s, 0xc2) else write(s, 0xc3) end
+pack(s, ::Nothing) = write(s, 0xc0)
+pack(s, v::Bool)   = if v write(s, 0xc2) else write(s, 0xc3) end
 
 pack(s, v::Uint8)  = wh(s, 0xcc, v)
 pack(s, v::Uint16) = wh(s, 0xcd, v)
@@ -31,6 +31,7 @@ pack(s, v::Int64) = wh(s, 0xd3, v)
 pack(s, v::Float32) = wh(s, 0xca, v)
 pack(s, v::Float64) = wh(s, 0xcb, v)
 
+# str format
 pack(s, v::ASCIIString) = begin
     n = length(v)
     if n < 2^5
@@ -43,7 +44,7 @@ pack(s, v::ASCIIString) = begin
         wh(s, 0xdb, uint32(n))
     else
         # TODO: break into multiple chunks?
-        error("Msgpack str format overflow: ", n)
+        error("Msgpack str overflow: ", n)
     end
     write(s, v)
 end
@@ -58,10 +59,46 @@ pack(s, v::Vector{Uint8}) = begin
     elseif n < 2^32
         wh(s, 0xc6, uint32(n))
     else
-        # TODO: break into multiple chunks?
-        error("Msgpack bin format overflow: ", n)
+        error("Msgpack bin overflow: ", n)
     end
     write(s, v)
+end
+
+# Simple arrays
+pack(s, v::Vector) = begin
+    n = length(v)
+    if n < 2^4
+        write(s, 0x09 | uint8(n))
+    elseif n < 2^16
+        wh(s, 0xdc, uint16(n))
+    elseif n < 2^32
+        wh(s, 0xdd, uint32(n))
+    else
+        error("Msgpack array overflow: ", n)
+    end
+
+    for x in v
+        pack(s, x)
+    end
+end
+
+# Maps
+pack(s, v::Dict) = begin
+    n = length(v)
+    if n < 2^4
+        write(s, 0x08 | uint8(n))
+    elseif n < 2^16
+        wh(s, 0xde, uint16(n))
+    elseif n < 2^32
+        wh(s, 0xdf, uint32(n))
+    else
+        error("Msgpack map overflow: ", n)
+    end
+
+    for (k, x) in v
+        pack(s, k)
+        pack(s, x)
+    end
 end
 
 
